fbcac2eda33cd29473debd473cca6101d20786d8
wangyanchao <wangyanchao@me.com>
2018-01-04 10:54:32 +0800

    my first add

---
+++ LearnDemo/LearnDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnDemo
+//
+//  Created by wangyanchao on 2018/1/4.
+//  Copyright © 2018年 wangyanchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnDemo/LearnDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnDemo
+//
+//  Created by wangyanchao on 2018/1/4.
+//  Copyright © 2018年 wangyanchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
